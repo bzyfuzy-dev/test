0125ca05d00cdd8bb91b60da3344fd1fbeb7439f
bzy from home <bzy.fuzy@gmail.com>
2022-07-06 17:24:39 +0800

    some bugs fixed

--- test.js
+++ test.js
@@ -1,5 +1,13 @@
 const puppeteer = require("puppeteer");
 const fs = require("fs");
+var formatter = new Intl.NumberFormat("en-US", {
+  style: "currency",
+  currency: "USD",
+
+  // These options are needed to round to whole numbers if that's what you want.
+  //minimumFractionDigits: 0, // (this suffices for whole numbers, but will print 2500.10 as $2,500.1)
+  //maximumFractionDigits: 0, // (causes 2500.99 to be printed as $2,501)
+});
 
 (async () => {
   let browser = await puppeteer.launch({ devtools: true });
@@ -13,8 +21,11 @@ const fs = require("fs");
   await page.goto("https://booking.com", {
     waitUntil: "networkidle2",
   });
-  const [hotelLink] = await page.$x("//a[contains(text(), 'Hotels')]");
-  await hotelLink.click();
+
+  const needs = await page.$('[data-ats="44"]');
+  const [hotelsLink] = await needs.$x("//a[contains(text(), 'Hotels')]");
+
+  await hotelsLink.click();
   const getNewPageWhenLoaded = async () => {
     return new Promise((x) =>
       browser.on("targetcreated", async (target) => {
@@ -43,26 +54,100 @@ const fs = require("fs");
 
   await newPage.waitForTimeout(4000);
 
-  const [viewHotel] = await newPage.$x(
-    `//*[@id="search_results_table"]/div[2]/div/div/div/div[6]/div[1]/div[1]/div[2]/div/div[1]/div[1]/div/div[1]/div/h3/a`
+  const [orderByPrice] = await newPage.$x(
+    `//*[@id="ajaxsrwrap"]/div[1]/div/div/div[2]/ul/li[3]/a`
   );
+  await orderByPrice.click();
 
-  viewHotel.click();
-  const hotelviewProm = getNewPageWhenLoaded();
-  const hotelPage = await hotelviewProm;
+  await newPage.waitForTimeout(4000);
+  await newPage.screenshot({ path: "./out/hotels.png", fullPage: true });
 
-  await hotelPage.waitForTimeout(4000);
-  let html = await hotelPage.content();
+  const hotelCards = await newPage.$$('[data-testid="property-card"]');
 
-  fs.writeFile("./output/hotel.html", html, (err) => {
-    if (err) {
-      console.error(err);
+  for (let i = 0; i < hotelCards.length; i++) {
+    let withMeal = await hotelCards[i].$('[data-testid="gallery-ribbon"]');
+    if (withMeal) {
+      let hotelTitle = await hotelCards[i].$('[data-testid="title"]');
+      let hotelT = await newPage.evaluate((el) => el.textContent, hotelTitle);
+      const hotelLink = await hotelCards[i].$('[data-testid="title-link"]');
+      hotelLink.click();
+      let hotelViewPromise = getNewPageWhenLoaded();
+      let hotelViewPage = await hotelViewPromise;
+      await hotelViewPage.waitForTimeout(4000);
+      const table = await hotelViewPage.$("#hprt-table");
+      let priceRows = await hotelViewPage.$$("#hprt-table tr");
+      ("prco-valign-middle-helper");
+      priceRows.shift();
+      let cheapest = {
+        index: 0,
+        price: 100000000,
+      };
+      for (let j = 0; j < priceRows.length; j++) {
+        let priceElem = await priceRows[j].$("span.prco-valign-middle-helper");
+        let priceString = await hotelViewPage.evaluate(
+          (el) => el.textContent,
+          priceElem
+        );
+        let price = Number(priceString.replace(/[^0-9.-]+/g, ""));
+
+        if (cheapest.price > price) {
+          cheapest.price = price;
+          cheapest.index = j;
+        }
+        // const price = priceRows[j].$eval;
+      }
+      priceRows[cheapest.index].screenshot({
+        path: `./out/${hotelT}-cheap.png`,
+      });
+
+      await table.screenshot({ path: `./out/${hotelT}.png` });
+      await hotelViewPage.waitForTimeout(5000);
+      await hotelViewPage.close();
     }
-    // file written successfully
-  });
+  }
 
-  const table = await hotelPage.$("#hprt-table");
-  //   console.log(prices[1][2].split("\n"));
-  await table.screenshot({ path: "./out/all-prices.png" });
+  // hotelCards.forEach(async (card) => {
+  //   let withBre = await card.$('[data-testid="gallery-ribbon"]');
+  //   if (withBre) {
+  //     let hotelTitle = await card.$('[data-testid="title"]');
+  //     let hotelT = await newPage.evaluate((el) => el.textContent, hotelTitle);
+  //     const hotelLink = await card.$('[data-testid="title-link"]');
+  //     console.log(hotelT);
+  //     console.log(hotelLink);
+  //     hotelLink.tap();
+  //     // await hotelLink.evaluate((a) => a.click());
+  //     let hotelViewPromise = getNewPageWhenLoaded();
+  //     let hotelViewPage = await hotelViewPromise;
+  //     await hotelViewPage.waitForTimeout(4000);
+
+  //     // const table = await hotelViewPage.$("#hprt-table");
+  //     // await table.screenshot({ path: `./out/${hotelT}.png` });
+  //     // await hotelViewPage.waitForTimeout(5000);
+  //     // await hotelViewPage.close();
+  //   }
+  // });
+
+  //   const [viewHotel] = await newPage.$x(
+  //     `//*[@id="search_results_table"]/div[2]/div/div/div/div[6]/div[1]/div[1]/div[2]/div/div[1]/div[1]/div/div[1]/div/h3/a`
+  //   );
+
+  //   await viewHotel.click();
+  //   const hotelviewProm = getNewPageWhenLoaded();
+  //   const hotelPage = await hotelviewProm;
+
+  //   await hotelPage.waitForTimeout(4000);
+
+  //   //   let html = await hotelPage.content();
+
+  //   //   fs.writeFile("./output/hotel.html", html, (err) => {
+  //   //     if (err) {
+  //   //       console.error(err);
+  //   //     }
+  //   //     // file written successfully
+  //   //   });
+
+  //   const table = await hotelPage.$("#hprt-table");
+  //   //   console.log(prices[1][2].split("\n"));
+  //   await table.screenshot({ path: "./out/all-prices.png" });
   await browser.close();
 })();
